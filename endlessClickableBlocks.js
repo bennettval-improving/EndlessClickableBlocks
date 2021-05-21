13486eee632135ccf4e4be3a43c6d6e73dbca638
Bennett Valentine <bennett.valentine@improving.com>
2021-05-21 14:49:40 -0500

    Endless underwater dogs

--- endlessClickableBlocks.js
+++ endlessClickableBlocks.js
@@ -1,22 +1,47 @@
-const addBlocksBtn = document.querySelector('#add-blocks-btn');
+const imageArray = [
+    'Images/underwaterDog2.jpg', 
+    'Images/underwaterDog1.jpg', 
+    'Images/underwaterDog3.jpg', 
+    'Images/underwaterDog4.jpg', 
+    'Images/underwaterDog5.jpg', 
+    'Images/underwaterDog6.jpg', 
+    'Images/underwaterDog7.jpg', 
+    'Images/underwaterDog8.jpg', 
+    'Images/underwaterDog9.jpg'];
 const targetDiv = document.querySelector('#target-div');
 
-addBlocksBtn.addEventListener('click', () => {
-    // get random number for number of blocks
-    let randomBlocks = Math.floor(Math.random() * 10);
+function addOneHundredDogs() {
+    for (let i = 0; i < 100; i++) {
+        let randomNum = Math.floor(Math.random() * 9);
+        let dog = document.createElement('img');
+        //dog.setAttribute('src', imageArray[randomNum]);
+        dog.src = imageArray[randomNum];
+        dog.classList.add('block');
+        targetDiv.appendChild(dog);
+    }
+}
 
-    for (let i = 0; i < randomBlocks; i++) {
-        // get random number
-        let randomNum = Math.floor(Math.random() * 1000);
-    
-        // create blocks
-        let block = document.createElement('div');
-        block.textContent = randomNum;
-        block.classList.add('block');
-        targetDiv.appendChild(block);
+window.addEventListener('scroll', () => {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        console.log('YOURE AT THE BOTTOM');
+        addOneHundredDogs();
     }
 });
 
-targetDiv.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('block')) evt.target.classList.toggle('selected');
-});
+addOneHundredDogs();
+
+// addBlocksBtn.addEventListener('click', () => {
+//     let randomBlocks = Math.floor(Math.random() * 10);
+
+//     for (let i = 0; i < randomBlocks; i++) {
+//         let randomNum = Math.floor(Math.random() * 1000);
+//         let block = document.createElement('div');
+//         block.textContent = randomNum;
+//         block.classList.add('block');
+//         targetDiv.appendChild(block);
+//     }
+// });
+
+// targetDiv.addEventListener('click', (evt) => {
+//     if (evt.target.classList.contains('block')) evt.target.classList.toggle('selected');
+// });
