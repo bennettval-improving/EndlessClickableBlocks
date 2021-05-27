2e22bf8fdf8f61bd68c06eda46ee093989266660
Bennett Valentine <bennett.valentine@improving.com>
2021-05-26 23:02:37 -0500

    Made some cosmetic and js changes with selecting blocks. 🎨 🟦

--- endlessClickableBlocks.js
+++ endlessClickableBlocks.js
@@ -1,51 +1,70 @@
-const imageArray = [
-    'Images/underwaterDog2.jpg', 
-    'Images/underwaterDog1.jpg', 
-    'Images/underwaterDog3.jpg', 
-    'Images/underwaterDog4.jpg', 
-    'Images/underwaterDog5.jpg', 
-    'Images/underwaterDog6.jpg', 
-    'Images/underwaterDog7.jpg', 
-    'Images/underwaterDog8.jpg', 
-    'Images/underwaterDog9.jpg'];
 const targetDiv = document.querySelector('#target-div');
 const detailsPopUp = document.querySelector('#details-pop-up');
 const popUpImage = document.querySelector('#pop-up-image');
 const popUpImageTitle = document.querySelector('.details-container > h3');
 const exitSign = document.querySelector('#exit-sign');
 const blocksContainer = document.querySelector('.blocks-container');
+const searchInput = document.querySelector('#search-input');
 
-function addOneHundredDogs() {
-    for (let i = 0; i < 100; i++) { 
-        let randomNum = Math.floor(Math.random() * 9);
-        let dog = document.createElement('img');
-        dog.src = imageArray[randomNum];
-        dog.classList.add('block');
-        targetDiv.appendChild(dog);
-    }
-}
-
-window.addEventListener('scroll', () => {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        console.log('YOURE AT THE BOTTOM');
-        addOneHundredDogs();
-    }
-});
+const baseArtUrl = 'https://api.harvardartmuseums.org/image';
+const apiKey = '?apikey=93be3097-51cc-4177-a633-040b80f209be';
 
 targetDiv.addEventListener('click', (evt) => {
-    if (evt.target.tagName === 'IMG') {
-        popUpImage.src = evt.target.src;
-        let srcArray = evt.target.src.split('/');
-        popUpImageTitle.textContent = srcArray[srcArray.length - 1];
-        detailsPopUp.style = 'display: inline-block';
-        blocksContainer.style = 'margin-right: 40%';
+    if (evt.target.id === 'target-div') return;
+
+    // remove selected class from previous block
+    let selectedArray = [...targetDiv.childNodes].filter(x => x.classList.contains('selected'));
+    if (selectedArray.length > 0) {
+        let selected = selectedArray[0]; // length should always be 1
+        selected.classList.remove('selected');
+    }
+
+    let block = evt.target.closest('.block');
+    popUpImage.src = block.childNodes[0].src;
+    detailsPopUp.style = 'display: inline-block';
+    blocksContainer.style = 'margin-right: 40%';
+    block.classList.add('selected');
+});
+
+exitSign.addEventListener('click', () => exitPopUp());
+
+searchInput.addEventListener('keyup', (evt) => {
+    if (evt.key === 'Enter') {
+        exitPopUp();
+        getArt(searchInput.value);
     }
 });
 
-exitSign.addEventListener('click', () => {
+function exitPopUp() {
     detailsPopUp.style = 'display: none';
     blocksContainer.style = 'margin-right: 10px';
-});
+}
 
-addOneHundredDogs();
+function getArt(search) {
+    fetch(`${baseArtUrl}${apiKey}&q=caption:${search}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            addArtToDocument(data);
+        });
+}
 
+function addArtToDocument(data) {
+    targetDiv.textContent = '';
+    data.records.forEach(record => {
+        let block = document.createElement('div');
+        block.classList.add('block');
+
+        let art = document.createElement('img');
+        art.src = record.baseimageurl;
+
+        let title =  document.createElement('div');
+        title.textContent = record.caption;
+
+        block.appendChild(art);
+        block.appendChild(title);
+        targetDiv.appendChild(block);
+    });
+}
+
+getArt("book");
